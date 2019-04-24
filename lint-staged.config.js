a53496c723eddee936e354dbfbb572d7aaafd3ab
Mykeal Kenny <mk@fireinjun.com>
2019-04-24 14:13:37 -0400

    added to lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  'src/**/*.js': ['npm run lint', 'npm run style', 'npm run test', 'npm run coverage', 'git add'],
+  linters: {
+    'src/**/*.js': ['npm run lint', 'npm run style', 'npm run test', 'npm run coverage', 'git add'],
+  },
 };
