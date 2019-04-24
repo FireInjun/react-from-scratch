a7ce011b2729f9f2adc8fa9308bc14482a8fc2a5
Mykeal Kenny <mk@fireinjun.com>
2019-04-24 04:41:40 -0400

    added boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import reactDOM from "react-dom";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import reactDOM from 'react-dom';
 
 const App = () => {
-  return
-  <div>Hello React, Webpack 4 & Babel 7!</div>
-}
+  return <div>Hello React, Webpack 4 & Babel 7!</div>;
+};
