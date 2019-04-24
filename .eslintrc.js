c20df9543c2c020c512397bcd73e3f778e75cf84
Mykeal Kenny <mk@fireinjun.com>
2019-04-24 13:56:30 -0400

    reformatted eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,15 @@
 module.exports = {
-  env: {
-    "es6": true,
-    "browser": true,
-    "node": true
-  },
-  extends: [
-    "airbnb",
-    "prettier",
-    "plugin:jest/recommended",
-    "jest-enzyme"
-  ],
-  parser: "babel-eslint",
-  plugins: [
-    "prettier",
-    "babel",
-    "import",
-    "jsx-a11y",
-    "react"
-  ],
-  rules: {
-    "prettier/prettier": ["error"],
-    "react/jsx-filename-extension": 0,
-    "no-unused-vars": 0
-  }
-}
+	env: {
+		es6: true,
+		browser: true,
+		node: true
+	},
+	extends: [ 'airbnb', 'prettier', 'plugin:jest/recommended', 'jest-enzyme' ],
+	parser: 'babel-eslint',
+	plugins: [ 'prettier', 'babel', 'import', 'jsx-a11y', 'react' ],
+	rules: {
+		'prettier/prettier': [ 'error' ],
+		'react/jsx-filename-extension': 0,
+		'no-unused-vars': 0
+	}
+};
